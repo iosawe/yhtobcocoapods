1a5270f6ffbda85da86683bd38d8d5d550b94a65
ZLL <liuleixiansen@163.com>
2021-12-29 14:20:07 +0800

    c

--- Scripts/netlify_deploy.sh
+++ Scripts/netlify_deploy.sh
@@ -2,11 +2,12 @@
 set -eo pipefail
 
 mkdir _site || true
-
+mkdir _site/Specs || true
 # generate sharded index
 bundle exec ruby Scripts/create_pods_and_versions_index.rb _site
 
-# add redirects to GitHub
-cp Scripts/netlify_redirects.txt _site/_redirects
-
+# add redirects to GitHub 我要注释掉
+#cp Scripts/netlify_redirects.txt _site/_redirects
+#我加的
+cp -r Specs/* _site/Specs/
 cp ./*.yml _site/
